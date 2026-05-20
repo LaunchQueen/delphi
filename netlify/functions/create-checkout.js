82f722585ce56d4795b3e5c9701224de5ec789d3
LaunchQueen <maureen@launchactually.com>
2026-05-20 13:20:34 -0400

    Integrate Stripe for checkout session creation

--- netlify/functions/create-checkout.js
+++ netlify/functions/create-checkout.js
@@ -1,3 +1,7 @@
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
 const HEADERS = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Headers": "Content-Type",
